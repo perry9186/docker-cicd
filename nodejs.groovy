9f1747d19f504467b0d5b3a708476dd57ff09842
EC2 Default User <ec2-user@ip-172-31-16-19.us-west-2.compute.internal>
2021-07-13 07:20:47 +0000

    first commit

---
+++ nodejs.groovy
@@ -0,0 +1,19 @@
+job('boilerplate') { // Job NAME
+    scm { // Configure Source control management 
+        git('git://github.com/perry9186/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@domain.com)
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell("npm install")
+    }
+}
+
